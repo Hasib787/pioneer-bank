3a143a5d01239082a69d9eece3f3988a2ab4e3ac
Hasib787 <mr.hasib45@gmail.com>
2021-01-27 21:49:23 +0600

    added bank deposite and balance

--- js/main.js
+++ js/main.js
@@ -1,7 +1,46 @@
+//Login button event handler
 const loginBtn = document.getElementById('login');
 loginBtn.addEventListener('click', function () {
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = 'none';
-    const transactionArea=document.getElementById('transaction-area');
-    transactionArea.style.display='block';
+    const transactionArea = document.getElementById('transaction-area');
+    transactionArea.style.display = 'block';
 });
+//deposit button event handler
+const depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener("click", function () {
+    const depositAmount = document.getElementById('depositAmount').value;
+    const depositNumber = parseFloat(depositAmount);
+
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+
+    document.getElementById('depositAmount').value = "";
+
+})
+function updateSpanText(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
+
+//withdraw button event handler
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click", function () {
+    const withdrawAmount = document.getElementById("withdrawAmount").value;
+    const withdrawNumber = parseFloat(withdrawAmount);
+
+    const currentWithdraw = document.getElementById('currentWithdraw').innerText;
+    const currentWithdrawNumber = parseFloat(currentWithdraw);
+    const totalWithdraw = withdrawNumber + currentWithdrawNumber;
+
+    document.getElementById('currentWithdraw').innerText = totalWithdraw;
+
+    const currentBalance = document.getElementById("currentBalance").innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = withdrawNumber - currentBalanceNumber;
+    document.getElementById("currentBalance").innerText = totalBalance;
+    
+    document.getElementById("withdrawAmount").value = "";
+})
